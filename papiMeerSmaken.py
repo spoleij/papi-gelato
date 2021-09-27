e496a55039bd6ae98366039945bbc0b95af4c48b
Steph Poleij <99033734@mydavinci.nl>
2021-09-27 21:08:40 +0200

    toppings

--- papiMeerSmaken.py
+++ papiMeerSmaken.py
@@ -6,11 +6,20 @@ count = None
 totaalHorentjes = None
 totaalBakjes = None
 totaalBolletjes = None
+slagroom = None
+sprinkels = None
+caramel = None
+prijsslagroom = None
+prijsCaramel = None
+prijsSprinkels = None
+aantalToppings = None
+totaalToppings = None
 #define variables hier / binnen de class. (zet je dit binnen de class dan krijg je een error)
 
 class Bestelling(object):                   #maak een class met object/self  zodat de variables binnen elkaars function gebruikt kunnen worden 
     count = 0
-    aardbei , chocolade , munt , vanille, totaalHorentjes, totaalBakjes , totaalBolletjes= (0,)*7
+    aardbei , chocolade , munt , vanille, totaalHorentjes, totaalBakjes , totaalBolletjes, slagroom, sprinkels, caramel= (0,)*10
+    prijsSlagroom , prijsCaramel , prijsSprinkels , aantalToppings , totaalToppings = (0,)*5
     def funcBolletjes (self):
         try:
             self.aantalBolletjes = int(input ('Hoeveel bolletjes wilt u?\n'))
@@ -59,14 +68,41 @@ class Bestelling(object):                   #maak een class met object/self  zod
         if self.houder in ['a', 'A','hoorntje' 'een hoortje' ]:
             self.houder = 'hoorntje'
             self.totaalHorentjes = self.totaalHorentjes + 1
-            self.funcMeer()
+            self.topping()
         elif self.houder in ['b','B''bakje''een bakje']:
             self.houder = 'bakje'
             self.totaalBakjes = self.totaalBakjes + 1
-            self.funcMeer()
+            self.topping()
         else:
             print ('Sorry, dat snap ik niet...')
             return self.funcHouder()
+    def topping (self):
+            self.welkeTop = input ("Wat voor topping wilt u?: \nA) Geen, \nB) Slagroom, \nC) Sprinkels \nD) Caramel Saus\n")
+
+            if self.welkeTop in ['a' ,'A', 'geen' 'Geen']:
+                self.funcMeer()
+
+            elif self.welkeTop in ['b', 'B', 'slagroom', 'Slagroom']:
+                self.slagroom = self.slagroom + 1
+                self.aantalToppings = self.aantalToppings + 1
+                
+            elif self.welkeTop in ['c', 'C', 'sprinkels', 'Sprinkels' ,'sprinkles', 'Sprinkles']:
+                self.sprinkels = self.sprinkels + 1
+                self.prijsSprinkels = self.prijsSprinkels + (self.aantalBolletjes * 0.30)
+                self.aantalToppings = self.aantalToppings + 1
+                
+            elif self.welkeTop in ['d' ,'D', 'caramel' ,'karamel', 'Caramel', 'Karamel', 'karamel saus' ,'Caramel saus']:
+                self.caramel = self.caramel + 1
+                self.aantalToppings = self.aantalToppings + 1
+                if self.houder == 'hoorntje':
+                    self.prijsCaramel = self.prijsCaramel + 0.60
+                if self.houder == 'bakje':
+                    self.prijsCaramel = self.prijsCaramel + 0.90
+            
+            else:
+                print ('Sorry, dat snap ik niet...')
+                return self.topping()    
+            self.funcMeer()       
 
     def funcMeer (self):
         self.meer = input (f'Hier is uw {self.houder} met {self.aantalBolletjes} bolletje(s). Wilt u nog meer bestellen? \n(J/N)\n')
@@ -83,15 +119,22 @@ class Bestelling(object):                   #maak een class met object/self  zod
         self.berekeningBol = self.totaalBolletjes * 1.10
         self.berekeningHor = self.totaalHorentjes * 1.25
         self.berekeningBak = self.totaalBakjes * 0.75
-        self.berekeningTot = self.berekeningBol + self.berekeningHor + self.berekeningBak
+        self.berekeningTot = self.berekeningBol + self.berekeningHor + self.berekeningBak + self.totaalToppings
+        self.prijsSlagroom = 0.50 * self.slagroom
+        #self.prijsSprinkels
+        #self.prijsCaramel
+        self.totaalToppings = self.prijsCaramel + self.prijsSlagroom + self.prijsSprinkels
         print (f"""                     
         ---------------["Papi Gelato"]---------------
 
-        Bolletjes	{self.totaalBolletjes} x $1.10  	= ${round(self.berekeningBol,2)}
+        Bolletjes	{self.totaalBolletjes} x $1.10  	= ${round(self.berekeningBol,2)}  
         Horentjes	{self.totaalHorentjes} x $1.25  	= ${round(self.berekeningHor,2)}
         Bakjes		{self.totaalBakjes} x $0.75       = ${round(self.berekeningBak,2)}
+        Topping         1 x ${self.totaalToppings}        = ${round(self.totaalToppings)}  
                         ---------- +
         Totaal				= ${round(self.berekeningTot)}                  \n   """)
+#AFRONDEN LUKT NOG NIET HELEMAAL WANT ALS IETS 0.50 IS KRIJG JE 0. NA DE =
+
 
 obj = Bestelling()                                              #maak object en call obj.function, de eerste
 obj.funcBolletjes()
